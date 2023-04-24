62a0322d83c37c5bfdd5b131ad46e4bfe6d2fe0c
Ivan <iv.cherepanov@yandex.ru>
2023-04-24 18:10:50 +0300

    Работа с панелью администратора Настроены бренды

--- src/http/animal_shop/brandApi.js
+++ src/http/animal_shop/brandApi.js
@@ -2,7 +2,7 @@ import {$authHost, $host, $springHost} from "../index";
 import jwt_decode from "jwt-decode"
 
 export const createBrand = async (brand) => {
-    const {data} = await $authHost.post('api/brand', brand)
+    const {data} = await $springHost.post('api/brand', brand)
     return data
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,30 @@
-import Admin from "./pages/Admin";
+import AdminShop from "./pages/AdminShop";
 import {
     ADMIN_PATH,
-    BASKET_PATH,
-    DEVICE_PATH, LICENSE_AG_PATH, LICENSE_PATH,
-    LOGIN_PATH, PRODUCT_PATH, PRODUCTS_PATH,
+    BASKET_PATH, BRAND_PATH,
+    DEVICE_PATH, ITEMTYPE_PATH, LICENSE_AG_PATH, LICENSE_PATH,
+    LOGIN_PATH, ORDER_PATH, PET_PATH, PRODUCT_PATH, PRODUCTS_PATH,
     REGISTRATION_PATH,
     SHOP_PATH,
-    TEST_PATH
+    TEST_PATH, USER_PATH
 } from "./utils/consts";
 import Basket from "./pages/Basket";
 import Shop from "./pages/old/Shop";
 import DevicePage from "./pages/old/DevicePage";
 import Auth from "./pages/Auth";
-import Test from "./pages/Test";
+import Brand from "./pages/Brand";
 import AgreementForm from "./pages/old/AgreementForm";
 import AgrementFormAgressive from "./pages/old/AgrementFormAgressive";
 import ProductPage from "./pages/ProductPage";
 import Products from "./pages/Products";
+import Test from "./pages/old/Test";
+import User from "./pages/User";
+import Pet from "./pages/Pet";
+import Item_type from "./pages/Item_type";
+import Order from "./pages/Order";
 
 export const authRoutes = [
-    {path: ADMIN_PATH, Component: Admin},
+    {path: ADMIN_PATH, Component: AdminShop},
     {path: BASKET_PATH, Component: Basket},
     //{ path: "*", element: Error, exact: false }
 ]
@@ -35,4 +40,9 @@ export const publicRoutes = [
     {path: LICENSE_AG_PATH, Component: AgrementFormAgressive},
     {path: PRODUCTS_PATH, Component: Products},
     {path: PRODUCT_PATH + '/:id', Component: ProductPage},
+    {path: BRAND_PATH, Component: Brand},
+    {path: USER_PATH, Component: User},
+    {path: PET_PATH, Component: Pet},
+    {path: ITEMTYPE_PATH, Component: Item_type},
+    {path: ORDER_PATH, Component: Order},
 ]
