6761f62b5aba1ee3b85a106cbb01d9e243bf61ba
Ivan <iv.cherepanov@yandex.ru>
2023-04-30 20:14:21 +0300

    Админская часть работает Pet панель работает корректно

--- src/http/animal_shop/petApi.js
+++ src/http/animal_shop/petApi.js
@@ -1,4 +1,5 @@
 import {$springHost} from "../index";
+import pet from "../../pages/admin/Pet";
 
 export const fetchPets = async () => {
     const {data} = await $springHost.get('api/pet/list')
@@ -8,5 +9,20 @@ export const fetchPets = async () => {
 
 export const getPetById = async (id) => {
     const {data} = await $springHost.get(`api/pet/${id}`)
+    return data
+}
+
+export const createPet = async (pet) => {
+    const {data} = await $springHost.post('api/pet/create', pet)
+    return data
+}
+export const deletePet = async (id) => {
+    const {data} = await $springHost.delete(`api/pet/${id}`)
+    return data
+}
+
+export const updatePet = async (params) => {
+    const {id, pet} = params;
+    const {data} = await $springHost.put(`api/pet/${id}`, pet)
     return data
 }
--- src/pages/admin/Pet.js
+++ src/pages/admin/Pet.js
@@ -1,33 +1,28 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
-import {fetchDevices, fetchTypes} from "../../http/deviceApi";
-import {deleteBrand, fetchBrands} from "../../http/brandApi";
 import {Button} from "react-bootstrap";
-import CreateBrand from "../../components/modals/CreateBrand";
-import UpdateBrand from "../../components/modals/UpdateBrand";
+import {deleteItemType, fetchItemTypes} from "../../http/animal_shop/itemTypeApi";
+import {deletePet, fetchPets} from "../../http/animal_shop/petApi";
+import Create from "../../components/modals/Pet/Create";
+import UpdatePet from "../../components/modals/Pet/Update";
 
 
-const Brand = observer(() => {
-    const {device} = useContext(Context)
-    const [brandVisible, setBrandVisible] = useState(false);
-    const [brandNewVisible, setBrandNewVisible] = useState(false);
-    const [selectedBrandId, setSelectedBrandId] = useState(null);
-    console.log(device.brands.map(brand => brand.name))
+const Pet = observer(() => {
+    const {product} = useContext(Context)
+    const [petVisible, setPetVisible] = useState(false);
+    const [petNewVisible, setPetNewVisible] = useState(false);
+    const [selectedPetId, setSelectedPetId] = useState(null);
+
 
     useEffect(()=>{
-        fetchBrands().then(data => device.setBrands(data))
-        fetchDevices(null, null, 1, 3).then(data => {
-                device.setDevices(data.rows)
-                device.setTotalCount(data.count)
-            }
-        )
-    }, []) // todo device.brands
+        fetchPets().then(data => product.setPets(data))
+    }, [])
 
-    const deleteBrandById = async (event, id) => {
+    const deletePetById = async (event, id) => {
         try {
             event.preventDefault();
-            await deleteBrand(id); // make DELETE request to API endpoint
+            await deletePet(id); // make DELETE request to API endpoint
         } catch (e) {
             console.error(e);
         }
@@ -36,64 +31,65 @@ const Brand = observer(() => {
     return (
         <div className="container" style={{ marginTop: "50px" }}>
             <div className="row">
-                <h1>Список брендов</h1>
+                <h1>Список животных</h1>
             </div>
             <div className="row">
                 <div className="col-lg-3">
                     <Button
                         variant={"outline-dark"}
                         className="mt-2"
-                        onClick={() => setBrandVisible(true)}
+                        onClick={() => setPetVisible(true)}
                     >
-                        Add brand
+                        Add pet
                     </Button>
-                    <CreateBrand
-                        show={brandVisible}
-                        onHide={() => setBrandVisible(false)}
+                    <Create
+                        show={petVisible}
+                        onHide={() => setPetVisible(false)}
                     />
                 </div>
             </div>
-            <table className="table table-striped table-bordered">
+            <table className="table table-striped table-bordered mt-3">
                 <thead className="table-dark">
                 <tr>
                     <th>Название</th>
-                    {/*<th>Величина скидки</th>*/}
                     <th>Действия</th>
                 </tr>
                 </thead>
                 <tbody>
-                {device.brands.map((brand) => (
+                {product.pets.map((pet) => (
 
-                    <tr key={brand.id}>
+                    <tr key={pet.id}>
 
-                        <td>{brand.name}</td>
-                        {/*<td>{brand.sale}</td>*/}
+                        <td>{pet.petName}</td>
                         <td>
-                            <div>
+                            <div  className="d-flex align-items-center">
                                 <Button
                                     variant={"outline-dark"}
-                                    className="mt-2"
+                                    className="mr-3"
                                     onClick={() => {
-                                        setBrandNewVisible(true);
-                                        setSelectedBrandId(brand.id);
+                                        setPetNewVisible(true);
+                                        setSelectedPetId(pet.id);
                                     }}
                                 >
-                                    Update brand
+                                    Update pet
                                 </Button>
-                                <UpdateBrand
-                                    show={brandNewVisible && selectedBrandId === brand.id}
-                                    onHide={() => setBrandNewVisible(false)}
-                                    brandId={selectedBrandId}
+                                <UpdatePet
+                                    show={petNewVisible && selectedPetId === pet.id}
+                                    onHide={() => setPetNewVisible(false)}
+                                    petId={selectedPetId}
                                 />
+                                <div className="m-lg-2"></div>
+                                <Button
+                                    className="ml-2"
+                                    variant="danger"
+                                    onClick={(event) => deletePetById(event, pet.id)}
+                                >
+                                    Удалить
+                                </Button>
+
                             </div>
 
-                            <a
-                                href={'/brand/del/${brand.id}'}
-                                className="btn btn-danger"
-                                onClick={(event) => deleteBrandById(event, brand.id)}
-                            >
-                                Удалить
-                            </a>
+
                         </td>
                     </tr>
 
@@ -106,4 +102,4 @@ const Brand = observer(() => {
     );
 });
 
-export default Brand;
+export default Pet;
