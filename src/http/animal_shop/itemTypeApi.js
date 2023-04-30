6542c4716f8c09bcc9559229ba59ddc40c5797dc
Ivan <iv.cherepanov@yandex.ru>
2023-04-30 20:00:45 +0300

    Админская часть работает Item Type панель работает корректно

--- src/http/animal_shop/itemTypeApi.js
+++ src/http/animal_shop/itemTypeApi.js
@@ -9,3 +9,18 @@ export const getItemTypeById = async (id) => {
     const {data} = await $springHost.get(`api/item_type/${id}`)
     return data
 }
+
+export const createItemType = async (itemType) => {
+    const {data} = await $springHost.post('api/item_type/create', itemType)
+    return data
+}
+export const deleteItemType = async (id) => {
+    const {data} = await $springHost.delete(`api/item_type/${id}`)
+    return data
+}
+
+export const updateItemType = async (params) => {
+    const {id, itemType} = params;
+    const {data} = await $springHost.put(`api/item_type/${id}`, itemType)
+    return data
+}
--- src/pages/admin/Item_type.js
+++ src/pages/admin/Item_type.js
@@ -6,28 +6,27 @@ import {deleteBrand, fetchBrands} from "../../http/brandApi";
 import {Button} from "react-bootstrap";
 import CreateBrand from "../../components/modals/CreateBrand";
 import UpdateBrand from "../../components/modals/UpdateBrand";
+import {fetchItems} from "../../http/animal_shop/itemApi";
+import {deleteItemType, fetchItemTypes} from "../../http/animal_shop/itemTypeApi";
+import Create from "../../components/modals/ItemType/Create";
+import Update from "../../components/modals/ItemType/Update";
 
 
 const Brand = observer(() => {
-    const {device} = useContext(Context)
-    const [brandVisible, setBrandVisible] = useState(false);
-    const [brandNewVisible, setBrandNewVisible] = useState(false);
-    const [selectedBrandId, setSelectedBrandId] = useState(null);
-    console.log(device.brands.map(brand => brand.name))
+    const {product} = useContext(Context)
+    const [itemTypeVisible, setItemTypeVisible] = useState(false);
+    const [itemTypeNewVisible, setItemTypeNewVisible] = useState(false);
+    const [selectedItemTypeId, setSelectedItemTypeId] = useState(null);
+
 
     useEffect(()=>{
-        fetchBrands().then(data => device.setBrands(data))
-        fetchDevices(null, null, 1, 3).then(data => {
-                device.setDevices(data.rows)
-                device.setTotalCount(data.count)
-            }
-        )
+        fetchItemTypes().then(data => product.setTypes(data))
     }, []) // todo device.brands
 
-    const deleteBrandById = async (event, id) => {
+    const deleteItemTypeById = async (event, id) => {
         try {
             event.preventDefault();
-            await deleteBrand(id); // make DELETE request to API endpoint
+            await deleteItemType(id); // make DELETE request to API endpoint
         } catch (e) {
             console.error(e);
         }
@@ -36,64 +35,65 @@ const Brand = observer(() => {
     return (
         <div className="container" style={{ marginTop: "50px" }}>
             <div className="row">
-                <h1>Список брендов</h1>
+                <h1>Список типов</h1>
             </div>
             <div className="row">
                 <div className="col-lg-3">
                     <Button
                         variant={"outline-dark"}
                         className="mt-2"
-                        onClick={() => setBrandVisible(true)}
+                        onClick={() => setItemTypeVisible(true)}
                     >
-                        Add brand
+                        Add item_type
                     </Button>
-                    <CreateBrand
-                        show={brandVisible}
-                        onHide={() => setBrandVisible(false)}
+                    <Create
+                        show={itemTypeVisible}
+                        onHide={() => setItemTypeVisible(false)}
                     />
                 </div>
             </div>
-            <table className="table table-striped table-bordered">
+            <table className="table table-striped table-bordered mt-3">
                 <thead className="table-dark">
                 <tr>
                     <th>Название</th>
-                    {/*<th>Величина скидки</th>*/}
                     <th>Действия</th>
                 </tr>
                 </thead>
                 <tbody>
-                {device.brands.map((brand) => (
+                {product.types.map((type) => (
 
-                    <tr key={brand.id}>
+                    <tr key={type.id}>
 
-                        <td>{brand.name}</td>
-                        {/*<td>{brand.sale}</td>*/}
+                        <td>{type.itemTypeName}</td>
                         <td>
-                            <div>
+                            <div  className="d-flex align-items-center">
                                 <Button
                                     variant={"outline-dark"}
-                                    className="mt-2"
+                                    className="mr-3"
                                     onClick={() => {
-                                        setBrandNewVisible(true);
-                                        setSelectedBrandId(brand.id);
+                                        setItemTypeNewVisible(true);
+                                        setSelectedItemTypeId(type.id);
                                     }}
                                 >
-                                    Update brand
+                                    Update type
                                 </Button>
-                                <UpdateBrand
-                                    show={brandNewVisible && selectedBrandId === brand.id}
-                                    onHide={() => setBrandNewVisible(false)}
-                                    brandId={selectedBrandId}
+                                <Update
+                                    show={itemTypeNewVisible && selectedItemTypeId === type.id}
+                                    onHide={() => setItemTypeNewVisible(false)}
+                                    itemTypeId={selectedItemTypeId}
                                 />
+                                <div className="m-lg-2"></div>
+                                <Button
+                                    className="ml-2"
+                                    variant="danger"
+                                    onClick={(event) => deleteItemTypeById(event, type.id)}
+                                >
+                                    Удалить
+                                </Button>
+
                             </div>
 
-                            <a
-                                href={'/brand/del/${brand.id}'}
-                                className="btn btn-danger"
-                                onClick={(event) => deleteBrandById(event, brand.id)}
-                            >
-                                Удалить
-                            </a>
+
                         </td>
                     </tr>
 
